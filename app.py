f8fe6d8d87cb6036f5f419f7ad008e47f3f80d10
piergiorgio di pasquale <pier98.dipa@gmail.com>
2023-02-22 13:50:06 +0100

    message

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) # instance of class Flask
 
 @app.route("/") # any website has a route. a part of the url after the url
 # this is going to match the empty route
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", debug = True)
