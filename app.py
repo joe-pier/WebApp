36b04bb59075a8d09e7d691db068463fa3367b28
piergiorgio di pasquale <pier98.dipa@gmail.com>
2023-02-23 12:19:15 +0100

    added cv link and aesthetics

--- app.py
+++ app.py
@@ -2,36 +2,36 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__) # instance of class Flask
 
-jobs = [
-    {
-    "id":3,
-    "title": "Data Analyst",
-    "company": "Google",
-    "jobdescription": "Python developer",
-    "location": "Milan",
-    "start": "01-01-2021",
-    "end" : "01-06-2021"
-    }
-    ,
-    {
-    "id":1,
-    "title": "Data & Analytics Consultant",
-    "company": "PwC",
-    "jobdescription": "mulesoft developer",
-    "location": "Milan",
-    "start": "06-06-2022",
-    "end" : "01-12-2022"
-    }
-    ,
-    {
-    "id":2,
-    "title": "Data Scientist",
-    "company": "BendingSpoons",
-    "jobdescription": "Python developer",
-    "location": "Milan",
-    "start": "01-01-2023"
-    }
-]
+jobs =  [
+        {
+        "id":3,
+        "title": "Data Analyst",
+        "company": "Google",
+        "jobdescription": "Python developer",
+        "location": "Milan",
+        "start": "01-01-2021",
+        "end" : "01-06-2021"
+        }
+        ,
+        {
+        "id":1,
+        "title": "Data & Analytics Consultant",
+        "company": "PwC",
+        "jobdescription": "mulesoft developer",
+        "location": "Milan",
+        "start": "06-06-2022",
+        "end" : "01-12-2022"
+        }
+        ,
+        {
+        "id":2,
+        "title": "Data Scientist",
+        "company": "BendingSpoons",
+        "jobdescription": "Python developer",
+        "location": "Milan",
+        "start": "01-01-2023"
+        }
+        ]
 
 @app.route("/") # any website has a route. a part of the url after the url
 # this is going to match the empty route
